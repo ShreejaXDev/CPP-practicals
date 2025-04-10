d41f94070d6679edaa853e634d39d0d4d9c8e447
Shreeja Upadhyay <shreejaupadhyaycspitce@gmail.com>
2025-04-10 21:35:19 +0530

    Update p1.1.cpp

--- p1.1.cpp
+++ p1.1.cpp
@@ -10,60 +10,212 @@ ability to view their account balance whenever required.
 The system must be designed using Object-Oriented Programming principles, focusing on 
 creating a simple and efficient solution to manage the accounts effectively. The system should 
 ensure that all account details are secure and accessible only through authorized methods. */
-    //this program is prepared by 24CE132_shreeja
+ //this program is prepared by 24CE132_shreeja
+
 
 #include<iostream>
 using namespace std;
+
 class bank
 {
+private:
+
+    string accholder;
+    int acc_no;
+    double balance,dep,w;
 
 public:
-    int acc_no;
-    char name[25];
-float bal,n,w,am;
+
+    void getdata()
+    {
+        cout<<endl<<"enter account holder name ";
+        cin>>accholder;
+
+        cout<<endl<<"enter acc number :";
+        cin>>acc_no;
+
+        cout<<endl<<"enter intial balance:";
+        cin>>balance;
 
 
-   void add() //function for addding amount to account
-   {
-       cout<<"\nenter account holder name :";
-       cin>>name;
-       cout<<"\nenter amount you want to add to your balance:";
-       cin>>am;
-       bal+=am;
-   }
-   void withdraw() //function for withdrawing money
-   {
-       cout<<endl<<"\nenter amount you want to withdraw";
-       cin>>w;
 
-       if(w>bal)
-       {
-           cout<<endl<<"\nyou cant withdraw this much money since your balance is less then that";
-       }
-   }
-   void balance() //function for checking current balance
-   {
-      if(w<=bal)
-      {
-          n=bal-w;
-          cout<<endl<<"current balance: "<<n;
-      }
-      else
-         cout<<endl<<"current balance: "<<bal;
-   }
-   };
-   int main()
-   {
+    }
 
-       bank bb;
-       bb.acc_no=12345;
-       bb.bal=5000;
-        cout<<"INTIAL BALANCE :"<<bb.bal;
-        bb.add();
-        bb.withdraw();
-        bb.balance();
+    int get_acc()
+    {
+        return acc_no;
+    }
 
-        cout<<endl<<endl<<"24CE132_shreeja";
+    void display()
+    {
+        cout<<endl<<"---------------------------------------------------------------------------";
+        cout<<endl<<"NAME :"<<accholder;
+        cout<<endl<<"ACC_NO. :"<<acc_no;
+        cout<<endl<<" BALANCE :"<<currentbalance();
+    }
 
-       return 0;
-   }
+
+    void deposite()
+    {
+    cout<<endl<<"enter how much money you want to deposite";
+    cin>>dep;
+
+    balance+=dep;
+
+    cout<<endl<<"you have deposited money successfully";
+    }
+
+    double currentbalance()
+    {
+        return balance;
+    }
+
+    void withdraw()
+    {
+        cout<<endl<<"enter amount you want to withdraw";
+        cin>>w;
+
+        if(w>balance)
+        {
+            cout<<endl<<"you cant withdraw these much amount since you have not sufficient balance :";
+
+        }
+        else
+        {
+            balance-=w;
+            cout<<endl<<"you have withdrawn money successfully";
+        }
+    }
+
+};
+
+
+int main()
+{
+   bank b[100];
+   int acc_find; //making account find
+   int choice; //for making choice
+   int num_acc=0; //for tracking account
+
+    //Printing menu
+    cout << "Enter 1 to add account details." << endl;
+    cout << "Enter 2 to deposit." << endl;
+    cout << "Enter 3 to withdraw." << endl;
+    cout << "Enter 4 to display current balance." << endl;
+    cout << "Else 5 to Exit." << endl;
+    cout << "===================================" << endl;
+
+
+while(true)
+{
+    cout<<endl<<"enter your choice :";
+    cin>>choice;
+
+    bool found=false;
+    switch(choice)
+    {
+    case 1: //for adding details
+        {
+            b[num_acc].getdata();
+            num_acc++;
+            break;
+        }
+
+    case 2://Case for depositing money
+        {
+            cout << "Enter the account number: ";
+            cin >> acc_find;//Taking input of Account no. to search
+
+            for (int i = 0; i < num_acc; i++)//Loop for matching account no. with entered account no.
+            {
+                if (b[i].get_acc() == acc_find)//If entered account matched with account no.
+                {
+                    b[i].deposite();//Calling DepositMoney function
+
+                    found = true;//Account no. matched with entered account no.
+
+                    break;//Break statement
+                }
+            }
+              if(! found)//If not...
+                {
+                    cout << "Account not found!" << endl;//Printing error message
+                }
+
+            break;//Break statement
+        }
+
+
+    case 3://Case for withdrawing money
+        {
+            cout << "Enter the account number: ";
+            cin >> acc_find;//Taking input of Account no. to search
+
+            for (int i = 0; i < num_acc; i++)//Loop for matching Account no. with entered account no.
+            {
+                if (b[i].get_acc() == acc_find)//If entered account matched with account no.
+                {
+                    b[i].withdraw();//Calling WithdrawMoney function
+
+                    found = true;//Account no. matched with entered account no.
+
+                    break;//Break statement
+                }
+            }
+
+            if(! found)//If not...
+                {
+                    cout << "Account not found!" << endl;//Printing error message
+                }
+
+            break;//Break statement
+        }
+
+        case 4://Case for displaying current balance
+        {
+            cout << "Enter the account number: ";
+            cin >> acc_find;//Taking input of Account no. to search
+
+            for (int i = 0; i < num_acc; i++)//Loop for matching Account no. with entered account no.
+            {
+                if (b[i].get_acc() == acc_find)//If entered account matched with account no.
+                {
+                    b[i].display();//Calling DisplayAccBalance function
+
+                    found = true;//Account no. matched with entered account no.
+
+                    break;//Break statement
+                }
+            }
+
+            if(! found)//If not...
+            {
+                cout << "Account not found!" << endl;//Printing error message
+            }
+
+            break;//Break statement
+        }
+        case 5:
+            {
+
+
+        return 0 ;
+
+            }
+
+        }
+    }
+
+
+
+
+
+
+
+
+}
+
+
+   
+          
+       
